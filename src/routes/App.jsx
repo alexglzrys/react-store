721a95813633919b1470a49ac77c186c60917a56
Alejandro González Reyes <alexglzrys@gmail.com>
2022-09-11 20:14:53 -0500

    páginas Home y NotFound

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "../containers/Layout";
 import { RecoveryPassword } from "../containers/RecoveryPassword";
 import { RecoveryPasswordSendEmail } from "../containers/RecoveryPasswordSendEmail";
+import { Home } from "../pages/Home";
+import { NotFound } from "../pages/NotFound";
 
 export const App = () => {
   return (
@@ -10,11 +12,13 @@ export const App = () => {
     <BrowserRouter>
       <Layout>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/recovery-password" element={<RecoveryPassword />} />
           <Route
             path="/recovery-password-send-email"
             element={<RecoveryPasswordSendEmail />}
           />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
     </BrowserRouter>
