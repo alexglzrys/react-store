3bbfb3f78e363e5a4dcdfab74241458cd6be3b9d
Alejandro González Reyes <alexglzrys@gmail.com>
2022-09-11 19:43:40 -0500

    CSS RecoveryPasswordSendEmail

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Layout } from "../containers/Layout";
 import { RecoveryPassword } from "../containers/RecoveryPassword";
+import { RecoveryPasswordSendEmail } from "../containers/RecoveryPasswordSendEmail";
 
 export const App = () => {
   return (
     <Layout>
       <RecoveryPassword />
+      <RecoveryPasswordSendEmail />
     </Layout>
   );
 };
--- src/containers/RecoveryPasswordSendEmail.jsx
+++ src/containers/RecoveryPasswordSendEmail.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/RecoveryPasswordSendEmail.scss";
 
 export const RecoveryPasswordSendEmail = () => {
   return (
