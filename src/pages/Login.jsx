85ba73c1e66bb72d8fb9a600e52110dea107c22f
Alejandro González Reyes <alexglzrys@gmail.com>
2022-09-16 20:34:19 -0500

    useRef y formularios

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../styles/Login.scss";
 import Logotipo from "../assets/logos/logo_yard_sale.svg";
 
 export const Login = () => {
+  // Generar una referencia hacia el formulario.
+  // Esta técnica la podemos usar cuando no deseamos generar un estado para el formulario.
+  const formRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    // Prevenir el comportamiento por defecto al enviar un formulario
+    e.preventDefault();
+    // Recuperar la data del formulario mediate formData,
+    // haciendo uso de la referencia que apunta al formulario.
+    // Es más amigable para el programador, y en ocasiones más directa para enviar los datos al backend
+    const formData = new FormData(formRef.current);
+    // Formater el cuerpo de la petición a enviar al servidor
+    const data = {
+      username: formData.get("email"),
+      password: formData.get("password"),
+    };
+    console.log(data);
+  };
+
   return (
     <div className="Login">
       <div className="Login-container">
         <img src={Logotipo} alt="logo" className="logo" />
-        <form action="/" className="form">
+        <form action="/" className="form" ref={formRef}>
           <label htmlFor="email" className="label">
             Email address
           </label>
           <input
             type="text"
             id="email"
+            name="email"
             placeholder="platzi@example.cm"
             className="input input-email"
           />
@@ -23,12 +43,14 @@ export const Login = () => {
           <input
             type="password"
             id="password"
+            name="password"
             placeholder="*********"
             className="input input-password"
           />
           <input
             type="submit"
             value="Log in"
+            onClick={handleSubmit}
             className="primary-button login-button"
           />
           <a href="/">Forgot my password</a>
